1578e783f529cf309d6060042c417b9bf2a9c941
Lokus <lukas.tarantl@gmail.com>
2021-05-03 11:45:34 +0200

    first setup steps for firebase -added config files -added folder "functions" !DO NOT EDIT! -added new node-modules - i will send them as they are not committed to the repository

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import Menu from './app/Menu';
 import { NavigationContainer } from '@react-navigation/native';
+import firebase from "firebase";
 
 export default function App() {
   return (
