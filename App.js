7af2aa13cd1b361fb0f481a3d69fc2a882ed6706
Lokus <lukas.tarantl@gmail.com>
2021-05-30 18:36:50 +0200

    merging android into testing

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-<<<<<<< HEAD
-import Menu from './app/src/main/java/com/dteam/medsched/Menu';
-=======
 import Menu from './app/Menu';
 import Welcome from './app/WelcomePage';
->>>>>>> origin/testing
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <NavigationContainer>
-<<<<<<< HEAD
-      <Menu />
-=======
       <Welcome />
->>>>>>> origin/testing
     </NavigationContainer>
   );
 }
