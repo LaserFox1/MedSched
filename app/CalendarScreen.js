aec5e535a74259848354f88b2dcfa609f6e86bb1
Manjana Auer <manjana.auer@chello.at>
2021-06-13 21:37:06 +0200

    Show medications according to weekdays on calendar

--- app/CalendarScreen.js
+++ app/CalendarScreen.js
@@ -3,17 +3,41 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Calendar } from "react-native-calendars";
 import ProfileLine from "./ProfileLine1";
+import firebase from "firebase";
 
 function CalendarScreen(props) {
   const [state, setState] = useState("Select a day to see more information!");
+
+  if(!firebase.apps.length){
+  firebase.initializeApp({
+    apiKey: "AIzaSyBPBiT_zEwB85zG1xODwDPjW0ZXp9DUMQs",
+    authDomain: "medsched-29619.firebaseapp.com",
+    projectId: "medsched-29619"
+  });
+  }else firebase.app();
+
+  var db = firebase.firestore();
+
   return (
     <View style={styles.container}>
       <ProfileLine />
       <View>
         <Calendar
           onDayPress={(day) => {
-            setState(day.dateString);
             console.log("selected day", day);
+            var s = "";
+            db.collection("users").doc("Paul").collection("storedMedication").get().then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    console.log(doc.id, " => ", doc.data());
+                    var dayArr = doc.data().days.split(',');
+                    for (var d of dayArr){
+                        if(d == new Date(day.dateString).getDay()){
+                            s += `${doc.id}: ${doc.data().times}\n`
+                            setState(s);
+                        }
+                    }
+                });
+            });
           }}
           onMonthChange={(month) => {
             console.log("month changed", month);
